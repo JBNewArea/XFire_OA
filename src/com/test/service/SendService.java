4f8ec0d83589fff1c46b4492643c78002657a8a2
guanzheyuan <834155417@qq.com>
2018-08-28 10:08:08 +0800

    submit all

--- src/com/test/service/SendService.java
+++ src/com/test/service/SendService.java
@@ -12,13 +12,59 @@ import net.sf.json.JSONSerializer;
 
 public class SendService implements ISendService {
 
+	
+	private String EMAILCODE="SMS_141582437";//邮件code
+	private String SCHEDULECODE="SMS_142385534";//日程code
+	private String TASKCODE = "SMS_142380576";//新任务code
+	private String NOTICECODE ="SMS_142385539";//新闻code
+	private String MEETINGCODE ="SMS_14235540";//会议code
+	private String SUBFLOWCODE = "SMS_142945395";//流程提交code
+	private String BACKFLOWCODE="SMS_142945405";//流程退回code
+	private String FUSINESEFLOWCODE="SMS_142950401";//流程委托提醒code
+	private String COPYFLOWCODE="SMS_142950398";//流程抄送提醒code
+	private String OVERFLOWCODE="SMS_142945398";//流程结束提醒code
+	private String PRESSCODE="SMS_142950397";//流程催办提醒code
 	public String SendMsg(String phone,String content) throws ClientException, InterruptedException {
-		
-		  Map<String, String> map = new HashMap<String, String>();
-	        String templateCode = "SMS_125115886";
-	        String signName = "江北新区行政审批局";
-	        String paramString = "{\"vcode\":\"7788\"}";
-	        
+		    String templateCode = "";
+		    String signName = "江北新区行政审批局";
+		    String paramString = "";
+	        System.out.println("手机号："+phone);
+	        System.out.println("内容："+content);
+		    if(content.indexOf("邮件")!=-1){
+		    	templateCode = EMAILCODE;
+		    	paramString = "{\"emailContent\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("日程")!=-1){
+		    	templateCode = SCHEDULECODE;
+		    	paramString = "{\"scheduleContent\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("任务")!=-1){
+		    	templateCode = TASKCODE;
+		    	paramString = "{\"taskContent\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("新闻")!=-1){
+		    	templateCode = NOTICECODE;
+		    	paramString = "{\"noticeContent\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("会议")!=-1){
+		    	templateCode = MEETINGCODE;
+		    	paramString = "{\"meetingContent\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("提交")!=-1){
+		    	templateCode = SUBFLOWCODE;
+		    	paramString = "{\"subflow\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("催办")!=-1){
+		    	templateCode = PRESSCODE;
+		    	paramString = "{\"press\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("完毕")!=-1){
+		    	templateCode = OVERFLOWCODE;
+		    	paramString = "{\"overflow\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("抄送")!=-1){
+		    	templateCode = COPYFLOWCODE;
+		    	paramString = "{\"copyflow\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("委托")!=-1){
+		    	templateCode = FUSINESEFLOWCODE;
+		    	paramString = "{\"fusineseflow\":\"'"+content+"'\"}";
+		    }else if(content.indexOf("退回")!=-1){
+		    	templateCode = BACKFLOWCODE;
+		    	paramString = "{\"backflow\":\"'"+content+"'\"}";
+		    }
+		    Map<String, String> map = new HashMap<String, String>();
 	        map.put("telphone", phone);
 	        map.put("templateCode", templateCode);
 	        map.put("signName", signName);
